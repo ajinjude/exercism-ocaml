296570fece80ca3ddc4269b684605cef6a65ecf2
Ajin Jude <ajin45@gmail.com>
2019-04-19 23:45:27 +0530

    Refactor anagram

--- anagram/anagram.ml
+++ anagram/anagram.ml
@@ -4,8 +4,7 @@ let sorted_list str = String.lowercase str
                       |> List.sort ~compare: Char.compare
 
 let anagrams str strings =
-  let not_equal x y = String.(String.lowercase x <> String.lowercase y)
-  in
-  let deduplicated_lst = List.filter ~f: (not_equal str) strings
-  in
-  List.filter ~f: (fun x -> (List.equal (sorted_list x) (sorted_list str) ~equal:Char.equal)) deduplicated_lst
+  let not_equal x y = String.(String.lowercase x <> String.lowercase y) in
+  let deduplicated_lst = List.filter ~f: (not_equal str) strings in
+  let str = sorted_list str in
+  List.filter ~f: (fun x -> (List.equal (sorted_list x) str ~equal:Char.equal)) deduplicated_lst
