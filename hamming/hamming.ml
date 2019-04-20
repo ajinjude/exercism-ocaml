6b66a0fd93b0ad43e1ed437035dabb43bd3a2a59
Ajin Jude <ajin45@gmail.com>
2019-04-21 02:39:20 +0530

    Refactor hamming

--- hamming/hamming.ml
+++ hamming/hamming.ml
@@ -1,15 +1,11 @@
 open Base
+open Base.Poly
+
 type nucleotide = A | C | G | T
 
-let equal (x,y) =
-  match (x,y) with
-  | (A,A) -> true
-  | (C,C) -> true
-  | (G,G) -> true
-  | (T,T) -> true
-  | _ -> false
+let equal (x,y) = x = y
 
 let hamming_distance strand1 strand2 =
   match List.zip strand1 strand2 with
   | None -> None
-  | Some xs -> Some (List.count ~f:(fun x -> not(equal x)) xs)
+  | Some xs -> Some (List.count ~f:(Fn.non equal) xs)
