af8fdb0e23a17f283d59a23f6791bfbc7a3747a5
Ajin Jude <ajin45@gmail.com>
2019-04-23 00:50:42 +0530

    Refactor balanced brackets

--- bracket-push/bracket_push.ml
+++ bracket-push/bracket_push.ml
@@ -1,24 +1,21 @@
 open Base
-open Base.Poly
 
 let clean str =
   let is_bracket = function '(' | '[' | '{' | ')' | ']' | '}' -> true | _ -> false in 
   String.filter ~f: is_bracket str |> String.to_list
 
-let pop bracket stack =
-  match stack with
-  | x::xs when x = bracket -> xs
-  | _ -> bracket::stack
+let pop bracket = function
+  | x::xs when Char.(x = bracket) -> xs
+  | _ -> [bracket]
+
+let left = function ')' -> '(' | ']' -> '[' | '}' -> '{' | x -> x
 
 let are_balanced str =
-  let rec aux stack char_list =
-    match char_list with
+  let rec aux stack  = function
     | [] -> stack
     | x::xs ->  match x with
                 | '(' | '{' | '[' -> aux (x::stack) xs
-                | ')' -> aux (pop '(' stack) xs
-                | ']' -> aux (pop '[' stack) xs
-                | '}' -> aux (pop '{' stack) xs
+                | ')' | ']' | '}' as x -> aux (pop (left x) stack) xs
                 | x -> x::stack
   in
-  List.is_empty (aux ([]) (clean str))
+  List.is_empty (aux [] (clean str))
